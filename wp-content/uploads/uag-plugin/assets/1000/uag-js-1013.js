bd8921f0fe4bb466b11758c5314d9c455c662a73
Syamsul Alam <admin@alampintar.org>
2024-07-13 08:59:15 +0700

    commit
    commit 1

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1013.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-272ec60b', {"layout":"number","heading":"Proyek Selesai","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":300,"totalNumber":300,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-462e4442', {"layout":"number","heading":"Pengiriman Bunga","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":700,"totalNumber":700,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-a63a79e9', {"layout":"number","heading":"Tim Professional","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":40,"totalNumber":300,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-d68f887e', {"layout":"number","heading":"Penghargaan Global","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":6,"totalNumber":300,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
